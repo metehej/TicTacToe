d45875c0c8f3131697cba673b6c0bbb68972b107
MateKre <68050161+MateKre@users.noreply.github.com>
2023-05-31 18:51:12 +0200

    final version

--- tictactoe-forms/tictactoe-forms/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ tictactoe-forms/tictactoe-forms/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -636,7 +636,7 @@ namespace tictactoe_forms {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/tictactoe-forms;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/tictactoe-forms;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
